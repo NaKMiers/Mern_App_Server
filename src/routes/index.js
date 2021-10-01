982c7b55ceac3c12edfcce2bce230b0c5e7b26b9
AnK <56575187+NaKMiers@users.noreply.github.com>
2021-10-02 00:44:55 +0700

    1212

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const postsRouter = require('./posts')
 
 function route(app) {
-   app.use('/posts', postsRouter)
-   app.get('/', (req, res) => {
-      res.send('<h1>Welcome to Mern App</h1>')
-   })
+   app.use('/', postsRouter)
+   // app.get('/', (req, res) => {
+   //    res.send('<h1>Welcome to Mern App</h1>')
+   // })
 }
 
 module.exports = route
