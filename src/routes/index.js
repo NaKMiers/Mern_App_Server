9dde3fd03edcc77c35c7dbdb19979d88786481eb
AnK <56575187+NaKMiers@users.noreply.github.com>
2021-10-02 13:04:20 +0700

    edit posts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const postsRouter = require('./posts')
 
 function route(app) {
-   app.use('/posts', postsRouter)
+   app.use('/posts', (req, res) => {
+      res.send('<h1 style="color: #4caf50">This is Posts</h1>')
+   })
 
    app.get('/', (req, res) => {
       res.send('<h1 style="color: #f44336">Welcome Mern App</h1>')
